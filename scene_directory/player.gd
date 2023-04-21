9ffa5da53df55c2b5c46c7fa9a9e39cce162078e
Brandon T. Wood <malceore@gmail.com>
2023-04-20 20:04:15 -0400

    added tank model to player, replaced camera, attempted tank controls.

--- scene_directory/player.gd
+++ scene_directory/player.gd
@@ -1,31 +1,26 @@
 extends CharacterBody3D
-
-
-const SPEED = 1.0
+const MOVEMENT_SPEED = 3.0
+const ROTATION_SPEED = 1.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
-#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	handle_input(delta)
 	move_and_slide()
+
+func handle_input(delta):
+	velocity -= Vector3(0, 0, 0)
+	if Input.is_action_pressed("Forward"):
+		velocity = -transform.basis.z * MOVEMENT_SPEED
+	elif Input.is_action_pressed("Back"):
+		velocity = transform.basis.z * MOVEMENT_SPEED
+	if Input.is_action_pressed("Right"):
+		rotate_y(-ROTATION_SPEED * delta)
+	elif Input.is_action_pressed("Left"):
+		rotate_y(ROTATION_SPEED * delta)
+
