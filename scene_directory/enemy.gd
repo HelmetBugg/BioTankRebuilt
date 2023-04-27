c0509e44bf18c3909638af45477b1c195e34657e
Brandon T. Wood <malceore@gmail.com>
2023-04-27 19:32:03 -0400

    added some basic enemy states. idle, death and pursue

--- scene_directory/enemy.gd
+++ scene_directory/enemy.gd
@@ -2,19 +2,33 @@ extends CharacterBody3D
 
 enum States {IDLE, PURSUE, ATTACK, DEATH}
 var currentState = States.IDLE
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var target;
+const SPEED = 0.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _physics_process(delta):
-	pass
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	if target != null:
+		velocity = -transform.basis.z * SPEED
+	move_and_slide()
 
-
+func _process(delta):
+	if currentState == States.PURSUE:
+		look_at(target.position, Vector3.UP)
+	elif currentState == States.IDLE:
+		pass
+	elif currentState == States.DEATH:
+		pass
 
 func _on_sight_area_area_entered(area):
 	if(area.name == "PlayerVisibilityArea"): #change to something less hacky
 		currentState = States.PURSUE
-	print(currentState)
+		target = area.get_parent()
+
+
+func _on_sight_area_area_exited(area):
+	if(area.name == "PlayerVisibilityArea" and area.get_parent().name == target.name): 
+		currentState = States.IDLE
+		target = null
