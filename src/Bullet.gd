e0d6bb4e5aaa6b26450b5f2721c6008a581af1cf
TheCrusty <zezoku@yahoo.com>
2023-04-27 21:05:23 -0400

    added splosions

--- src/Bullet.gd
+++ src/Bullet.gd
@@ -1,18 +1,32 @@
 extends Area3D
 signal exploded
+var exploding = false;
 @export var speed = 40
 @export var damage = 1
 @export var localGravity = Vector3.DOWN * 0.5
+
+@onready var _bullet_splosion = $splosion
+
 var type = "physical or.. whatever."
 var velocity = Vector3.ZERO
 
 func _process(delta):
-	velocity += localGravity * delta
-	look_at(transform.origin + velocity.normalized(), Vector3.UP)
-	transform.origin += velocity * delta
+	if !exploding:
+		velocity += localGravity * delta
+		look_at(transform.origin + velocity.normalized(), Vector3.UP)
+		transform.origin += velocity * delta
+	else:
+		velocity = Vector3(0, 0, 0)
 
 func _on_body_entered(body):
 	emit_signal("exploded", transform.origin)
-	if body.type == "enemy":
+	if "type" in body and body.type == "enemy":
 		body.hit(damage, type)
+	exploding = true
+	$MeshInstance3D.visible = false
+	$splosion.visible = true
+	_bullet_splosion.play("splode")
+
+
+func _on_splosion_animation_finished():
 	queue_free()
