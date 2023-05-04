dfaa611bb76a97dd801270c83abc719ab381b5a8
Brandon T. Wood <malceore@gmail.com>
2023-05-04 19:55:40 -0400

    tuned up controls, but broke bullet drop.

--- src/player.gd
+++ src/player.gd
@@ -1,28 +1,25 @@
-extends CharacterBody3D
-const MOVEMENT_SPEED = 3.0
+extends RigidBody3D
+
+const MOVEMENT_SPEED = 4500
 const ROTATION_SPEED = 1.0
 const JUMP_VELOCITY = 4.5
+var slew_acceleration: float = 1
+var max_slew: float = 100
 var bullet = preload("res://scene_directory/bullet.tscn")
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 	handle_input(delta)
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	move_and_slide()
 
 func handle_input(delta):
-	velocity = Vector3(0, 0, 0)
 	if Input.is_action_pressed("Forward"):
-		velocity = -transform.basis.z * MOVEMENT_SPEED
+		self.apply_central_force(self.get_global_transform().basis.z.normalized() * -MOVEMENT_SPEED)
 	elif Input.is_action_pressed("Back"):
-		velocity = transform.basis.z * MOVEMENT_SPEED
-	if Input.is_action_pressed("Right"):
-		rotate_y(-ROTATION_SPEED * delta)
-	elif Input.is_action_pressed("Left"):
-		rotate_y(ROTATION_SPEED * delta)
+		self.apply_central_force(self.get_global_transform().basis.z.normalized() * MOVEMENT_SPEED)
+	if Input.is_action_pressed("Left"):
+		self.angular_velocity.y = min(self.angular_velocity.y + slew_acceleration, max_slew)
+	elif Input.is_action_pressed("Right"):
+		self.angular_velocity.y = max(self.angular_velocity.y - slew_acceleration, -max_slew)
+
 	if Input.is_action_just_pressed("Shoot"):
 		var b = bullet.instantiate()
 		owner.add_child(b)
